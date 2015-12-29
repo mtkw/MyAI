9426782b1f1854e4737ca2374f105151885b2a7d
Mati <mati.kaweczynski@gmail.com>
2015-12-29 11:25:58 +0100

    Budowa Populacji w formie binarnej Konwersja formy binarnej do tablicy intów int[] Wybór populacji do dalszego rozwoju Implementacja metod krzyżowania, reprodukcji i mutacji

--- AI_AlgorytmGenetyczny/src/genetiks/test.java
+++ AI_AlgorytmGenetyczny/src/genetiks/test.java
@@ -1,10 +1,41 @@
 package genetiks;
 
+import java.util.Arrays;
+
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Fenotyp fenotyp = new Fenotyp();
+		Population pop = new Population();
+		MetodyGenetyczne mg = new MetodyGenetyczne();
+
+		fenotyp.createStringFenotyp(0, 31);
+		
+		fenotyp.convertStringToIntTab(fenotyp.getFenotypStringList());
+		
+		pop.createPopulation(fenotyp.getFenotypIntTab(), 2);
+		
+		System.out.println("Wielkoœæ Populacji :" + pop.getPopulation().size());
+		for(int[] row: pop.getPopulation()){
+			System.out.println("Osobnik : " + Arrays.toString(row));
+		}
+		
+		System.out.println("Próba reprodukcji");
+		mg.reprodukcja(pop.getPopulation().get(0));
+		System.out.println("Rodzic : " + Arrays.toString(pop.getPopulation().get(0)));
+		System.out.println("Potomek : " + Arrays.toString(mg.getPotomek()));
+		
+		System.out.println("Próba krzy¿owania");
+		System.out.println("Rodzic 1 : " + Arrays.toString(pop.getPopulation().get(0)));
+		System.out.println("Rodzic 2 : " + Arrays.toString(pop.getPopulation().get(1)));
+		mg.krzyzowanie(pop.getPopulation().get(0), pop.getPopulation().get(1));
+		for(int[] row: mg.getPotomkowieLista()){
+			System.out.println("Potomek : " + Arrays.toString(row));
+		}
+		System.out.println("Próba mutacji");
+		mg.mutacja(pop.getPopulation().get(0));
+		
 	}
 
 }
